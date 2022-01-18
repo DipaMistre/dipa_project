c9af27c02538207a210ca3924cf5c4450d981f3c
DipaMistre <1822250@iub.edu.bd>
2022-01-19 00:04:48 +0600

    zdsfd

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,95 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.purple[100],
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('I am MINI'),
-          backgroundColor: Colors.purple[600],
-        ),
-        body: const Align(
-          alignment: Alignment.topLeft,
-          child: Image(
-            height: 200,
-            image: AssetImage(
-              'images/R.png',
+        backgroundColor: Colors.purple[300],
+        body: SafeArea(
+            child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 60.0,
+              backgroundImage: AssetImage('images/me.jpg'),
             ),
-          ),
-        ),
+            Text(
+              'Dipa Mitro',
+              style: TextStyle(
+                fontFamily: 'TheNautigal',
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Flutter Developer',
+              style: TextStyle(
+                fontFamily: 'MoonDance',
+                fontSize: 30.0,
+                color: Colors.blueAccent[100],
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.all(8.0),
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.phone,
+                    size: 30.0,
+                    color: Colors.purple,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    '+880 171 297 375',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'MoonDance',
+                        fontSize: 20.0),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.all(8.0),
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    size: 30.0,
+                    color: Colors.purple,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    'dipamitro63@gmail.com',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontFamily: 'MoonDance',
+                        fontSize: 30.0),
+                  )
+                ],
+              ),
+            )
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
